193dd5a4e5be1bb98e3e998d3a290be1f3332b9e
NexisSis <Nexis.sis@gmail.com>
2018-10-10 13:09:06 +0300

    finished video animate

--- src/blocks/video/video.js
+++ src/blocks/video/video.js
@@ -30,43 +30,46 @@
 //     'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8'
 // );
 
+$(document).ready(function(){
+  if($(document).width>450){
+    //open video animation
+    $('.video__item-1').click(function(){
+      $('.video__item-1').addClass('h-50 w-100');
+      $('.video__item-2').addClass('h-50 w-0 o-0' );
+      $('.video__item-3').addClass('h-0 o-0');
+      $('.video__item-4').addClass('h-0 o-0');
+      $('.video__button').addClass('o-1');
+    });
+    $('.video__item-2').click(function(){
+      $('.video__item-1').addClass('h-50 w-0 o-0' );
+      $('.video__item-2').addClass('h-50 w-100');
+      $('.video__item-3').addClass('h-0 o-0');
+      $('.video__item-4').addClass('h-0 o-0');
+      $('.video__button').addClass('o-1');
+    });
+    $('.video__item-3').click(function(){
+      $('.video__item-1').addClass('h-0 o-0');
+      $('.video__item-2').addClass('h-0 o-0' );
+      $('.video__item-3').addClass('w-100 h-50');
+      $('.video__item-4').addClass('w-0 h-50 o-0');
+      $('.video__button').addClass('o-1');
+    });
 
-//open video animation
-$('.video__item-1').click(function(){
-  $('.video__item-1').addClass('h-50 w-100');
-  $('.video__item-2').addClass('h-50 w-0 o-0' );
-  $('.video__item-3').addClass('h-0 o-0');
-  $('.video__item-4').addClass('h-0 o-0');
-  $('.video__button').addClass('o-1');
-});
-$('.video__item-2').click(function(){
-  $('.video__item-1').addClass('h-50 w-0 o-0' );
-  $('.video__item-2').addClass('h-50 w-100');
-  $('.video__item-3').addClass('h-0 o-0');
-  $('.video__item-4').addClass('h-0 o-0');
-  $('.video__button').addClass('o-1');
-});
-$('.video__item-3').click(function(){
-  $('.video__item-1').addClass('h-0 o-0');
-  $('.video__item-2').addClass('h-0 o-0' );
-  $('.video__item-3').addClass('w-100 h-50');
-  $('.video__item-4').addClass('w-0 h-50 o-0');
-  $('.video__button').addClass('o-1');
-});
+    $('.video__item-4').click(function(){
+      $('.video__item-1').addClass('h-0 o-0');
+      $('.video__item-2').addClass('h-0 o-0' );
+      $('.video__item-3').addClass('w-0 h-50 o-0');
+      $('.video__item-4').addClass('w-100 h-50');
+      $('.video__button').addClass('o-1');
+    });
 
-$('.video__item-4').click(function(){
-  $('.video__item-1').addClass('h-0 o-0');
-  $('.video__item-2').addClass('h-0 o-0' );
-  $('.video__item-3').addClass('w-0 h-50 o-0');
-  $('.video__item-4').addClass('w-100 h-50');
-  $('.video__button').addClass('o-1');
-});
-
-//show all videos animation
-$('.video__button').click(function(){
-  $('.video__item-1').removeClass('h-0 w-0 o-0 h-50 w-100');
-  $('.video__item-2').removeClass('h-0 w-0 o-0 h-50 w-100');
-  $('.video__item-3').removeClass('h-0 w-0 o-0 h-50 w-100');
-  $('.video__item-4').removeClass('h-0 w-0 o-0 h-50 w-100');
-  $('.video__button').removeClass('o-1');
+    //show all videos animation
+    $('.video__button').click(function(){
+      $('.video__item-1').removeClass('h-0 w-0 o-0 h-50 w-100');
+      $('.video__item-2').removeClass('h-0 w-0 o-0 h-50 w-100');
+      $('.video__item-3').removeClass('h-0 w-0 o-0 h-50 w-100');
+      $('.video__item-4').removeClass('h-0 w-0 o-0 h-50 w-100');
+      $('.video__button').removeClass('o-1');
+    });
+  }
 });
